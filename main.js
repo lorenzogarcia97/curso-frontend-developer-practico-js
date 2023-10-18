9a8348bc8af31c993d4a2d50e1accdcabe778e40
Lorenzo García <lorenzo.garciay97@gmail.com>
2023-10-18 17:04:10 -0300

    boton menu mbile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const navEMAIL = document.querySelector('.navbar-email');
 const desktopMENU = document.querySelector('.desktop-menu');
+const navHamIcon = document.querySelector('.menu');
+const mobMENU = document.querySelector('.mobile-menu');
 
 navEMAIL.addEventListener('click', toggleDesktopMenu);
+navHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     console.log('click')
     desktopMENU.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    console.log('click')
+    mobMENU.classList.toggle('inactive');
 }
